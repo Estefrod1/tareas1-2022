43c3ad9f6d7baaacefdd62991b54d4ef44e26aa2
Estefrod1 <101477494+Estefrod1@users.noreply.github.com>
2022-03-18 16:54:48 -0400

    sumar impares

---
+++ impares.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class impares {
+    public static void main(String[] args) {
+        Scanner text = new Scanner (System.in);
+        System.out.println("dame un numero");
+        int a, result;
+        a = text.nextInt();
+        int b = -1;
+        int suma = 0;
+        String cadena = "";
+        for (int i = 0; i < a; i++) {
+            b = b + 2;
+            suma = suma + b;
+            char d = (char) (b + '0');
+            cadena = cadena + d + "+";
+        }
+        System.out.println(cadena + "=" + b);
+    }
+}
