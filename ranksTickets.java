5dc090a70c283a0a221d618e088355fd6ca1cfd3
Estefrod1 <101477494+Estefrod1@users.noreply.github.com>
2022-03-23 18:50:23 -0400

    luego agrega

---
+++ ranksTickets.java
@@ -0,0 +1,8 @@
+public class RanksTickets {
+    String[] ranks = new String[5];
+    ranks[0] = "rango mayor";
+    ranks[1] = "rango mayor";
+    ranks[2] = "rango intermedio";
+    ranks[3] = "rango menor";
+    ranks[4] = "rango bajo";
+}
